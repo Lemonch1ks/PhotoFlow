1cc6f86740c15307b80e281da7cf8a21e44e578b
Oleksandr Dvirko <dvalex02@gmail.com>
2026-06-11 18:18:43 +0300

    created all nessesary models for the project with initial migrations

--- flow/models.py
+++ flow/models.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from photoflow.settings import AUTH_USER_MODEL
+from photoflow import settings
+
 
 
 class User(AbstractUser):
     role = models.CharField(max_length=100)
     pass
+
 
 
 class StudioRoom(models.Model):
@@ -24,10 +26,22 @@ class Service(models.Model):
 
 
 class Booking(models.Model):
-    client = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
-    photographer = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+    client = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="client_bookings",
+    )
+
+    photographer = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        related_name="photographer_bookings",
+    )
+
     studio_room = models.ForeignKey(StudioRoom, on_delete=models.CASCADE)
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)
     status = models.CharField(max_length=100)
     comment = models.TextField()
+
+
